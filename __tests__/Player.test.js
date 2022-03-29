3428d159dc279feb16d21b8ba4b5bf767f87d14e
Dan N <dn691997@gmail.com>
2022-03-28 21:24:27 -0700

    another commit

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,5 @@
 const { test, expect } = require('@jest/globals');
+const exp = require('constants');
 const Player = require('../lib/Player.js');
 const Potion = require('../lib/Potion.js');
 
@@ -60,4 +61,31 @@ test("subtracts from player's health", () => {
 	player.reduceHealth(99999);
 
 	expect(player.health).toBe(0);
+});
+
+test("gets player's attack value", () => {
+	const player = new Player('Dave');
+	player.strength = 10;
+
+	expect(player.getAttackValue()).toBeGreaterThanOrEqual(5);
+	expect(player.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('adds a potion to the inventory', () => {
+	const player = new Player('Dave');
+	const oldCount = player.inventory.length;
+
+	player.addPotion(new Potion());
+
+	expect(player.inventory.length).toBeGreaterThan(oldCount);
+});
+
+test('use a potion from inventory', () => {
+	const player = new Player('Dave');
+	player.inventory = [new Potion(), new Potion(), new Potion()];
+	const oldCount = player.inventory.length;
+
+	player.usePotion(1);
+
+	expect(player.inventory.length).toBeLessThan(oldCount);
 });
